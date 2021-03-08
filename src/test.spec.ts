a3278ba700dcede2d57ec679ceb155d4b4c31d7e
adrienboulle <adrien.boulle@gadz.org>
2021-03-08 18:52:35 +0100

    ci: add parser test

---
+++ src/test.spec.ts
@@ -0,0 +1,11 @@
+import { Parser } from './access-logs/parser';
+
+describe('parse', () => {
+  it('should parse simple and return one log', () => {
+    const parser = new Parser('./src/access-logs/examples/sample.log');
+
+    const logs = parser.parse();
+
+    expect(logs.length).toBe(1);
+  });
+});
