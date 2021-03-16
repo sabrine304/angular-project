d56ae90f4a5dc124c427c70185fd7ca73271cd19
Remi Sezille <remi.sezille-de-mazancourt@myre.fr>
2021-03-16 10:55:07 +0100

    -r Adrien

--- src/binary-criteria/README.md
+++ src/binary-criteria/README.md
@@ -2,15 +2,16 @@
 
 /!\ Read carefully
 
-This test is aimed to see how you search through data, how you manage async calls, and undefined criteria depth.
+This test goal is to see how you search through data, how you manage async calls, and undefined criteria depth.
 
-- The `criteria.spec.ts` file should not be modified at all, as well as `criteria.json`, `criteria2.json` and `data.sample.json`.
-- The `_sleep` method should not be modified, and related calls to the method should remain in their location.
-- We consider that all keys (genre, year, platforms and hasExpansion) are fetched asynchronously.
-  (for example let's imagine we need to do an API call to get the information, symbolized by the _sleep() method call)
+- The `criteria.json`, `criteria2.json` and `data.sample.json` files should not be modified at all.
+- The `_sleep` method should not be modified, and related calls to the method should remain at their current location.
+- We consider that all values (genre, year, platforms and hasExpansion) are fetched asynchronously.
+  (for example let's imagine we need to do an API call to get the information, mocked by the _sleep() method call)
 - Thus, those calls may have unpredictable execution time (random timeout in the _sleep method)
 
-You can modify the class as you wish: add/update/delete attributes, same for methods...
+Aside from the `_sleep` method and calls, you can modify the service as you wish: adding/updating/deleting attributes, methods...
+You are also free to add tests.
 
 The goal is to implement the `process` function.
 
@@ -18,6 +19,5 @@ The spec may be lacking some explanations: feel free to make assumptions and to 
 
 ## Key points
 
-- Every criterion can be asynchronous
 - Binary criteria can have unlimited depth (we can chain as `$or` or `$and` as we want)
 - The root of the "criteria" key can be seen as an "and"
--- src/binary-criteria/criteria.spec.ts
+++ src/binary-criteria/criteria.spec.ts
@@ -2,87 +2,89 @@ import { performance } from 'perf_hooks';
 
 import { CriteriaService } from './criteria.service';
 
-describe('process', () => {
-  it('should process the first sample', async () => {
-    const criteriaService = new CriteriaService('./data.sample.json');
+describe('do not modify', () => {
+  describe('process', () => {
+    it('should process the first sample', async () => {
+      const criteriaService = new CriteriaService('./data.sample.json');
 
-    const result = await criteriaService.process('./criteria.json');
+      const result = await criteriaService.process('./criteria.json');
 
-    const names = ['Warcraft 3', 'Age of Empire 2'];
+      const names = ['Warcraft 3', 'Age of Empire 2'];
 
-    expect(result.length).toBe(2);
-    expect(names.includes(result[0].name)).toBeTrue();
-    expect(names.includes(result[1].name)).toBeTrue();
+      expect(result.length).toBe(2);
+      expect(names.includes(result[0].name)).toBeTrue();
+      expect(names.includes(result[1].name)).toBeTrue();
+    });
+
+    it('should process the second sample', async () => {
+      const criteriaService = new CriteriaService('./data.sample.json');
+
+      const result = await criteriaService.process('./criteria2.json');
+
+      const names = ['Final Fantasy 10'];
+
+      expect(result.length).toBe(1);
+      expect(names.includes(result[0].name)).toBeTrue();
+    });
   });
 
-  it('should process the second sample', async () => {
-    const criteriaService = new CriteriaService('./data.sample.json');
+  describe('getGenre', () => {
+    it('should retrieve the genre after X seconds', async () => {
+      const criteriaService = new CriteriaService('./data.sample.json');
 
-    const result = await criteriaService.process('./criteria2.json');
+      const beforeCall = performance.now();
 
-    const names = ['Final Fantasy 10'];
+      await criteriaService.getGenre(0);
 
-    expect(result.length).toBe(1);
-    expect(names.includes(result[0].name)).toBeTrue();
+      const elapsedTime = (performance.now() - beforeCall) / 1000;
+
+      expect(elapsedTime).toBeGreaterThanOrEqual(0.05);
+      expect(elapsedTime).toBeLessThanOrEqual(1);
+    });
   });
-});
 
-describe('getGenre', () => {
-  it('should retrieve the genre after X seconds', async () => {
-    const criteriaService = new CriteriaService('./data.sample.json');
+  describe('getYear', () => {
+    it('should retrieve the year after X seconds', async () => {
+      const criteriaService = new CriteriaService('./data.sample.json');
 
-    const beforeCall = performance.now();
+      const beforeCall = performance.now();
 
-    await criteriaService.getGenre(0);
+      await criteriaService.getYear(0);
 
-    const elapsedTime = (performance.now() - beforeCall) / 1000;
+      const elapsedTime = (performance.now() - beforeCall) / 1000;
 
-    expect(elapsedTime).toBeGreaterThanOrEqual(0.05);
-    expect(elapsedTime).toBeLessThanOrEqual(1);
+      expect(elapsedTime).toBeGreaterThanOrEqual(0.05);
+      expect(elapsedTime).toBeLessThanOrEqual(1);
+    });
   });
-});
 
-describe('getYear', () => {
-  it('should retrieve the year after X seconds', async () => {
-    const criteriaService = new CriteriaService('./data.sample.json');
+  describe('getPlatforms', () => {
+    it('should retrieve the platforms after X seconds', async () => {
+      const criteriaService = new CriteriaService('./data.sample.json');
 
-    const beforeCall = performance.now();
+      const beforeCall = performance.now();
 
-    await criteriaService.getYear(0);
+      await criteriaService.getPlatforms(0);
 
-    const elapsedTime = (performance.now() - beforeCall) / 1000;
+      const elapsedTime = (performance.now() - beforeCall) / 1000;
 
-    expect(elapsedTime).toBeGreaterThanOrEqual(0.05);
-    expect(elapsedTime).toBeLessThanOrEqual(1);
+      expect(elapsedTime).toBeGreaterThanOrEqual(0.1);
+      expect(elapsedTime).toBeLessThanOrEqual(1);
+    });
   });
-});
 
-describe('getPlatforms', () => {
-  it('should retrieve the platforms after X seconds', async () => {
-    const criteriaService = new CriteriaService('./data.sample.json');
+  describe('hasExpansion', () => {
+    it('should retrieve if it has an expansion after X seconds', async () => {
+      const criteriaService = new CriteriaService('./data.sample.json');
 
-    const beforeCall = performance.now();
+      const beforeCall = performance.now();
 
-    await criteriaService.getPlatforms(0);
+      await criteriaService.hasExpansion(0);
 
-    const elapsedTime = (performance.now() - beforeCall) / 1000;
+      const elapsedTime = (performance.now() - beforeCall) / 1000;
 
-    expect(elapsedTime).toBeGreaterThanOrEqual(0.1);
-    expect(elapsedTime).toBeLessThanOrEqual(1);
-  });
-});
-
-describe('hasExpansion', () => {
-  it('should retrieve if it has an expansion after X seconds', async () => {
-    const criteriaService = new CriteriaService('./data.sample.json');
-
-    const beforeCall = performance.now();
-
-    await criteriaService.hasExpansion(0);
-
-    const elapsedTime = (performance.now() - beforeCall) / 1000;
-
-    expect(elapsedTime).toBeGreaterThanOrEqual(0.1);
-    expect(elapsedTime).toBeLessThanOrEqual(1);
+      expect(elapsedTime).toBeGreaterThanOrEqual(0.1);
+      expect(elapsedTime).toBeLessThanOrEqual(1);
+    });
   });
 });
